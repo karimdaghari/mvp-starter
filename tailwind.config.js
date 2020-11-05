3eca1e9ac51177a16a78391a46a862db8c4c0464
Karim Daghari <karim_daghari@outlook.com>
2020-11-05 23:14:31 +0100

    add components folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
-  purge: ["./pages/*.tsx"],
+  purge: ["./pages/*.tsx", "./components/*.tsx"],
   theme: {
     extend: {}
   },
