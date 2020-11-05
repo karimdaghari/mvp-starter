9b41bb55802c282ff3f09eeee3f8648ec8ebfbc3
Karim Daghari <karim_daghari@outlook.com>
2020-11-05 08:51:49 +0100

    extract layout into its own component

---
+++ components/Layout.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+export default function Layout({ children }: Props) {
+  return (
+    <div className="container">
+      <header className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold">
+          <Link href="/">MVP Starter</Link>
+        </h1>
+        <button className="px-6 py-2 font-medium uppercase border-2 border-orange-600 rounded-md hover:text-white hover:bg-orange-600">
+          Sign up
+        </button>
+      </header>
+
+      {children}
+
+      <footer className="flex items-center justify-center p-4 space-x-4 border border-t">
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/twitter.svg" width={30} height={30} />
+        </a>
+        <a
+          href="https://facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/facebook.svg" width={30} height={30} />
+        </a>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/instagram.svg" width={30} height={30} />
+        </a>
+      </footer>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import Layout from "../components/Layout";
 import "../styles/tailwind.css";
+import type { AppProps } from "next/app";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
