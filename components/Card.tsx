457559d0160e546f6b7c40374f15db2c26703ac0
Karim Daghari <karim_daghari@outlook.com>
2020-11-07 22:27:23 +0100

    .

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,10 +5,10 @@ interface Props extends React.ComponentPropsWithoutRef<"div"> {
 export default function Card({ className, children, title, ...props }: Props) {
   return (
     <div
-      className="max-w-lg p-4 border border-white rounded-md shadow-md"
+      className="max-w-lg p-4 border border-gray-300 rounded-md shadow-lg"
       {...props}
     >
-      <h1 className="mb-4 text-xl font-medium">{title}</h1>
+      <h1 className="mb-4 text-2xl font-medium">{title}</h1>
       {children}
     </div>
   );
--- pages/setup.tsx
+++ pages/setup.tsx
@@ -1,66 +1,88 @@
-import { Fragment } from "react";
 import Card from "../components/Card";
 import Input from "../components/Input";
 
 export default function Setup() {
   return (
-    <Fragment>
-      <main className="max-w-xl mx-auto space-y-4">
-        <Card id="identity" title="Identity">
-          <form className="space-y-4">
-            <div className="flex flex-col space-y-2">
-              <label htmlFor="name" className="text-base font-medium">
-                Name *
-              </label>
-              <Input placeholder="MVP starter" id="name" />
+    <>
+      <main className="max-w-lg p-4 mx-auto my-8 space-y-4">
+        <Card title="Setup">
+          <section id="identity">
+            <h2 className="mt-12 mb-4 text-lg font-medium">Identity</h2>
+            <div className="space-y-4">
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="name">Name *</label>
+                <Input placeholder="MVP starter" id="name" name="name" />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="logo">Logo</label>
+                <Input id="logo" name="logo" placeholder="Link" />
+              </div>
             </div>
-            <div className="flex flex-col space-y-2">
-              <span className="text-base font-medium">Logo *</span>
-              <Input placeholder="Link" />
-            </div>
-          </form>
-        </Card>
+          </section>
 
-        <Card id="content" title="Content">
-          <form>
-            <div>
-              <label htmlFor="tagline">Tagline *</label>
-              <Input
-                placeholder="Craft your MVP in under 5 minutes!"
-                id="tagline"
-              />
+          <section id="content">
+            <h2 className="mt-12 mb-4 text-lg font-medium">Copy</h2>
+            <div className="space-y-4">
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="tagline">Tagline *</label>
+                <Input
+                  placeholder="Craft your MVP in under 5 minutes!"
+                  id="tagline"
+                  name="tagline"
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="copy">Content *</label>
+                <textarea
+                  name="copy"
+                  id="copy"
+                  rows={5}
+                  className="p-2 border rounded-md"
+                />
+              </div>
+              <div className="flex flex-col space-y-4">
+                <label htmlFor="media">Media (image only)</label>
+                <Input id="media" name="media" placeholder="Link" />
+              </div>
             </div>
-            <div>
-              <label htmlFor="copy">Content *</label>
-              <textarea id="copy" rows={10} />
-            </div>
-            <div>
-              <p>Media *</p>
-              <p>Type</p>
-              <button>Image</button>
-              <button>Video</button>
-              <Input placeholder="Link" />
-            </div>
-          </form>
-        </Card>
+          </section>
 
-        <Card id="social" title="Social">
-          <form>
-            <div>
-              <label htmlFor="facebook">Facebook</label>
-              <Input placeholder="https://facebook.com/your.production.name" />
+          <section id="social">
+            <h2 className="mt-12 mb-4 text-lg font-medium">Social</h2>
+
+            <div className="space-y-4">
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="facebook">Facebook</label>
+                <Input
+                  placeholder="https://facebook.com/your.production.name"
+                  id="facebook"
+                  name="facebook"
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="instagram">Instagram</label>
+                <Input
+                  placeholder="https://instagram.com/your.product.name"
+                  id="instagram"
+                  name="instagram"
+                />
+              </div>
+              <div className="flex flex-col space-y-2">
+                <label htmlFor="twitter">Twitter</label>
+                <Input
+                  placeholder="https://twitter.com/your_product_name"
+                  id="twitter"
+                  name="twitter"
+                />
+              </div>
             </div>
-            <div>
-              <label htmlFor="instagram">Instagram</label>
-              <Input placeholder="https://instagram.com/your.product.name" />
-            </div>
-            <div>
-              <label htmlFor="twitter">Twitter</label>
-              <Input placeholder="https://twitter.com/your_product_name" />
-            </div>
-          </form>
+          </section>
+          <div className="flex justify-end space-x-3">
+            <button>Preview</button>
+            <button>Save</button>
+          </div>
         </Card>
       </main>
-    </Fragment>
+    </>
   );
 }
